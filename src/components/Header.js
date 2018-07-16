21eccac96ad1250be2527b62f0aacc9063b9f041
Julien Usson <usson.julien@gmail.com>
2018-07-16 21:52:53 +0200

    :sparkles: github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import styled from 'react-emotion'
+import Github from 'icons/Github'
 
 const Container = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   backgroundColor: '#2196F3',
-  color: '#fff',
-  fontSize: 24,
-  fontWeight: 700,
   paddingTop: 15,
   paddingRight: 30,
   paddingBottom: 15,
   paddingLeft: 30,
 })
 
-export default () => <Container>Continuous integration</Container>
+const Title = styled('div')({
+  color: '#fff',
+  fontSize: 24,
+  fontWeight: 700,
+})
+
+const GithubIcon = styled(Github)({
+  height: 32,
+  color: '#fff',
+})
+
+export default () => (
+  <Container>
+    <Title>Continuous integration</Title>
+    <a
+      href="https://github.com/JulienUsson/zz2-f5-cours"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <GithubIcon />
+    </a>
+  </Container>
+)
