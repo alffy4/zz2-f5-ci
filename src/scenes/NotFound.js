ea054d4fddbf7a47e4d3bc1f39ed323657f4b4cd
Julien Usson <usson.julien@gmail.com>
2018-07-16 19:02:58 +0200

    :lipstick: using emotion js

--- src/scenes/NotFound.js
+++ src/scenes/NotFound.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'react-emotion'
+
+const Container = styled('div')({
+  width: '100vw',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const Text = styled('div')({
+  textAlign: 'center',
+  fontSize: '48px',
+  color: '#263238',
+})
 
 export default () => (
-  <div className="notFound-container">
-    <div className="notFound-text">404 - Not Found</div>
-  </div>
+  <Container>
+    <Text>404 - Not Found</Text>
+  </Container>
 )
